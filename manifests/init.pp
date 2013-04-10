e812b4acaa48472e63710d04c32b7f4f87f87bb5
mohamedhadrouj-vv <mohamed.hadrouj@viavoo.com>
2013-04-10 16:27:09 +0200

    added roles list as class param

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,4 +1,4 @@
-class scout ($display_name=$hostname, $user='scout', $key) {
+class scout ($display_name=$hostname, $roles_list='', $user='scout', $key) {
 
   package {['scout', 'SystemTimer', 'elif', 'request-log-analyzer']:
       ensure   => 'installed',
@@ -9,9 +9,13 @@ class scout ($display_name=$hostname, $user='scout', $key) {
     $option_displayname = "--name='${display_name}'"
   }
 
+  if $roles_list != '' {
+    $option_roles = "--roles ${roles_list}"
+  }
+
   cron {
     'scout':
-      command   => "/usr/bin/scout ${option_displayname} ${key}",
+      command   => "/usr/bin/scout ${option_displayname} ${option_roles} ${key}",
       user	    => $user,
       require   => Package['scout'],
   }
